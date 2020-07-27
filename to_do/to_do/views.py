4e5945811f29eb6f3854b3de25f91b396175e59c
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-27 15:01:06 +0200

    modify the class Register to include exceptions

--- to_do/to_do/views.py
+++ to_do/to_do/views.py
@@ -7,8 +7,47 @@ class HelloWorld(APIView):
         return Response('HELLO WORLD from Django.')
 
 
-# VIew class to register user
+# View class to register user
 class Register(APIView):
     def post(self, request):
-        data = request.data
-        return Response(request.data)
+        # The order of required params is important as they are used to setvariables by index
+        required_params = ['username', 'password', 'email']
+        try:
+            data = request.data
+            # Checking if all the required parameters are available in data
+            if all(key in data for key in required_params):
+                try:
+                    user_name = self.validate_required_input(required_params[0], data[required_params[0]])
+                    password = self.validate_required_input(required_params[1], data[required_params[1]])
+                    email = self.validate_required_input(required_params[2], data[required_params[2]])
+                except ValidationError as er:
+                    return Response({"error": str(er.messages[0])}, status=status.HTTP_400_BAD_REQUEST)
+
+
+                # Input is now considered valid
+                # Creating user object to store in db
+                new_user = User()
+                new_user.username = user_name
+                new_user.password = make_password(password)
+                new_user.email = email
+
+                # Trying to setup optional parameters if available
+                try:
+                    new_user.first_name = data['firstname'] if data['firstname'] is not None else ""
+                except KeyError:
+                    print("Error while parsing firstname ")
+                try:
+                    new_user.last_name = data['lastname'] if data['lastname'] is not None else ""
+                except KeyError:
+                    print("Error while parsing lastname ")
+
+                new_user.save()
+
+                return Response({"status": "Success"}, status=status.HTTP_201_CREATED)
+
+            else:
+                return Response({"error": "Required param(s) missing, Please include and retry again"}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as exp:
+            print("Unexpected exception ocurred: "+str(exp))
+            return Response({"error": "Unexpected error occurred, please report this to Admin"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
