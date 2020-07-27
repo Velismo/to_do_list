a9193715ddd207401b27c9868b0a677ad18bc063
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-27 14:07:46 +0200

    added the class Register

--- to_do/to_do/views.py
+++ to_do/to_do/views.py
@@ -5,3 +5,10 @@ from rest_framework.response import Response
 class HelloWorld(APIView):
     def get(self, request):
         return Response('HELLO WORLD from Django.')
+
+
+# VIew class to register user
+class Register(APIView):
+    def post(self, request):
+        data = request.data
+        return Response(request.data)
