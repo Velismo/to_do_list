3a6e2c2153a1d1d7c43016ff71019e6a145896ff
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-07-27 13:30:30 +0200

    installed django

---
+++ todo/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Juan Manuel\Documents\OneDrive\Documentos\python_scripts\Proyectos\to_do_list\todo\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
